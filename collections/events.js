ea572bc0822dcf842217720b11a0f0838b8e4961
erasaur <erasaur@gmail.com>
2014-10-24 11:54:56 -0700

    Add schema, hooks, permissions and methods for events

--- collections/events.js
+++ collections/events.js
@@ -1,59 +1,186 @@
-EventsModel = new Meteor.Collection("events");
+var Schema = {};
 
-Octagon.Events = {
-  create: function (id, name, date, time, description, location, money, slots) {
-    EventsModel.insert({"id": id, "name": name, "date": date, "time": time, "description": description, "location": location, "money": money, "slots": slots, "members": [], "finalized": false});
+Schema.EventInfo = new SimpleSchema({
+  name: {
+    type: String
   },
-  addUrl: function (id, url) {
-    EventsModel.update({_id: EventsModel.findOne({"id": id})['_id']}, {$set: {"image": url}});
+  date: {
+    type: Date
   },
-  update: function (id, new_id, name, date, time, description, location, money, slots) {
-    EventsModel.update({_id: EventsModel.findOne({"id": id})['_id']}, {$set: {"id": new_id, "name": name, "date": date, "time": time, "description": description, "location": location, "money": money, "slots": slots}});
+  description: {
+    type: String
   },
-  editUrl: function (id, url) {
-    EventsModel.update({_id: EventsModel.findOne({"id": id})['_id']}, {$set: {"image": url}});
+  location: {
+    type: String
   },
-  addMember: function (id, member, memberid) {
-    EventsModel.update({_id: EventsModel.findOne({"id": id})['_id']}, {$push: {"members": {"name": member, "id": memberid}}});
-    EventsModel.update({_id: EventsModel.findOne({"id": id})['_id']}, {$inc: {"slots": -1}});
-  },
-  removeMember: function (id, member) {
-    EventsModel.update({_id: EventsModel.findOne({"id": id})['_id']}, {$pull: {"members": {"name": member}}});
-    EventsModel.update({_id: EventsModel.findOne({"id": id})['_id']}, {$inc: {"slots": 1}});
-  },
-  finalize: function (id) {
-    EventsModel.update({_id: EventsModel.findOne({"id": id})['_id']}, {$set: {"finalized": true}});
-  },
-  delete: function(id) {
-    EventsModel.remove({"_id": id});
-  }
-}
-
-EventsModel.allow({
-  insert: function (userId, doc) {
-    if(!userId) return false;
-
-    var officer = Meteor.users.findOne({"_id": userId}).profile.officer;
-    return (userId && officer);
-  },
-  update: function (userId, doc, fields, modifier) {
-    if(!userId) return false; //not logged in, return false (don't allow)
-
-    var officer = Meteor.users.findOne({"_id": userId}).profile.officer;
-    if (userId && officer) {
-      return true;
-    } else {
-      var allowed = ["members", "slots"]; //fields allowed to be changed
-        if (_.difference(fields, allowed).length)
-          return false; //don't allow change to another field
-
-        return true;
-    }
-  },
-  remove: function (userId, doc) {
-    if(!userId) return false;
-
-    var officer = Meteor.users.findOne({"_id": userId}).profile.officer;
-    return (userId && officer); //only if officer & logged in, allow
+  cost: { // omitted if zero
+    type: Number,
+    optional: true
   }
 });
+
+Schema.Events = new SimpleSchema({
+  _id: {
+    type: String,
+    optional: true
+  },
+  userId: {
+    type: String
+  },
+  createdAt: {
+    type: Date
+  },
+  info: {
+    type: Schema.EventInfo
+  },
+  members: {
+    type: [String],
+    optional: true
+  },
+  slots: { // omitted if unlimited
+    type: Number,
+    optional: true
+  },
+  finalized: {
+    type: Boolean
+  },
+  imageUrl: {
+    type: String
+  }
+});
+
+Events = new Mongo.Collection("events");
+Events.attachSchema(Schema.Events);
+
+Events.allow({
+  update: function (userId, event, fields) {
+    if (!userId) return false;
+
+    if (isAdminById(userId))
+      return true;
+  
+    var allowed = ["members", "slots"]; //fields allowed to be changed
+    return (_.difference(fields, allowed).length === 0);
+  },
+  remove: isAdminById
+});
+
+Events.before.insert(function (userId, doc) {
+  if (Meteor.isServer && doc.description)
+    doc.description = sanitize(marked(doc.description));
+});
+
+Events.before.update(function (userId, doc, fields, modifier, options) {
+  // sanitize before update
+  if (Meteor.isServer && modifier.$set && modifier.$set.description) {
+    modifier.$set = modifier.$set || {};
+    modifier.$set.description = sanitize(marked(modifier.$set.description));
+  }
+});
+
+Meteor.methods({
+  createEvent: function (event) {
+    var user = Meteor.user();
+    var userId = this.userId;
+
+    if (!user || !isAdmin(user))
+      throw new Meteor.Error('no-permission', getError('no-permission'));
+
+    var eventObj = {
+      userId: userId,
+      createdAt: new Date(),
+      info: event.info,
+      members: [],
+      finalized: false,
+      imageUrl: event.imageUrl
+    };
+
+    eventObj._id = Events.insert(eventObj);
+
+    // TODO: send notifications
+
+    return eventObj._id;
+  },
+  updateEvent: function (event) {
+    var user = Meteor.user();
+    var eventId = event._id;
+
+    if (!user || !isAdmin(user))
+      throw new Meteor.Error('no-permission', getError('no-permission'));
+
+    Events.update(eventId, { $set: event });
+  },
+  finalizeEvent: function (eventId) {
+    var user = Meteor.user();
+
+    if (!user || !isAdmin(user))
+      throw new Meteor.Error('no-permission', getError('no-permission'));
+
+    Events.update(eventId, { $set: { 'finalized': true } });
+  },
+  deleteEvent: function (eventId) {
+    var user = Meteor.user();
+
+    if (!user || !isAdmin(user))
+      throw new Meteor.Error('no-permission', getError('no-permission'));
+
+    Events.remove(eventId);      
+  },
+  attendEvent: function (eventId) {
+    var user = Meteor.user();
+    var userId = this.userId;
+
+    if (!user || !canAttendEvent(user))
+      throw new Meteor.Error('logged-out', getError('logged-out'));
+
+    var eventObj = Events.findOne(eventId);
+    if (typeof eventObj === 'undefined')
+      throw new Meteor.Error('not-exists', getError('not-exists'));
+
+    if (eventObj.finalized)
+      throw new Meteor.Error('already-finalized', getError('already-finalized'));
+
+    if (_.contains(eventObj.members, userId))
+      throw new Meteor.Error('already-attending', getError('already-attending'));
+
+    if (eventObj.slots <= 0)
+      throw new Meteor.Error('already-full', getError('already-full'));
+
+    Events.update(eventId, { $addToSet: { 'members': userId }, $inc: { 'slots': -1 } });
+  },
+  unattendEvent: function (eventId) {
+    var user = Meteor.user();
+    var userId = this.userId;
+
+    if (!user || !canAttendEvent(user))
+      throw new Meteor.Error('logged-out', getError('logged-out'));
+
+    var eventObj = Events.findOne(eventId);
+    if (typeof eventObj === 'undefined')
+      throw new Meteor.Error('not-exists', getError('not-exists'));
+
+    if (eventObj.finalized)
+      throw new Meteor.Error('already-finalized', getError('already-finalized'));
+
+    if (!_.contains(eventObj.members, userId))
+      throw new Meteor.Error('not-attending', getError('not-attending'));
+
+    Events.update(eventId, { $pull: { 'members': userId }, $inc: { 'slots': 1 } });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
