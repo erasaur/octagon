f47985c11cf15d55480f583f5847eca5db9cf024
erasaur <erasaur@gmail.com>
2014-10-25 16:45:22 -0700

    Switch publish package, fix events publication

--- server/publish/events.js
+++ server/publish/events.js
@@ -2,5 +2,12 @@ Meteor.publish('events', function (limit) {
   if (limit > Events.find().count()) {
     limit = 0;
   }
-  return Events.find({}, { limit: limit, sort: { 'date': -1 } });
+
+  publishWithRelations(this, Events.find({}, { limit: limit }), function (id, doc) {
+    this.cursor(Meteor.users.find({ '_id': { $in: doc.members } }, { fields: { 'profile': 1 } }));
+  });
+
+  return this.ready();
+
+  // return Events.find({}, { limit: limit, sort: { 'date': -1 } });
 });
