dafa6d48276eb456619b4a7c4448e870eaa7f43a
erasaur <erasaur@gmail.com>
2014-10-24 18:27:36 -0700

    Implement collection fs uploading and editing pictures

--- client/views/admin/pictures.js
+++ client/views/admin/pictures.js
@@ -2,61 +2,72 @@ var currentPicture;
 
 Template.pictures.helpers({
   pictures: function () {
-    return Pictures.find({});
+    return Pictures.find();
   }
 });
 
 Template.pictures.events({
-  'click #addPicture': function (event, template) {
-    var file = template.find('#createPicture').files[0],
-      caption = template.find('#createCaption').value,
-      featured = template.find('#createFeatured').checked;
-        
-    if(file && caption){
-        reader.onload = function (event) {
-          Octagon.Pictures.create(event.target.result, caption, featured);
-        }
-        reader.readAsDataURL(file);
-
-        alert("Success! The picture has been added.");
-        $('#addPictureModal').modal('hide');
-    } else {
-      alert("Please fill in all the fields!");
-    }
-  },
-  'click .editPictureButton': function (event, template) {
-    currentPicture = this;
+  'click #js-create': function (event, template) {
+    var user = Meteor.user();
+    var file = template.find('#js-create-picture').files[0];
+    var caption = template.find('#js-create-caption').value;
+    var featured = template.find('#js-create-featured').checked;
     
-    template.find('#editCaption').value = this.caption;
-  },
-  'click #savePicture': function (event, template) {
-    var reader = new FileReader();
-    var file = template.find('#editPicture').files[0],
-      caption = template.find('#editCaption').value,
-      featured = template.find('#editFeatured').checked;
+    if (!user || !isAdmin(user))
+      return alert(getError('no-permission'));
 
-    if(caption) {
-      if(file) {
-        reader.onload = function (event) {
-          Octagon.Pictures.update(currentPicture.source, event.target.result, caption, featured);
-          }
-          reader.readAsDataURL(file);
+    if (!stripHTML(caption))
+      return alert(getError('no-caption'));
 
-          alert("Success! The picture has been edited.");
-          $('#editPictureModal').modal('hide');
-      } else {
-        Octagon.Pictures.update(currentPicture.source, currentPicture.source, caption, featured);
+    if (typeof file === 'undefined')
+      return alert(getError('no-picture'));
 
-        alert("Success! The picture has been edited.");
-          $('#editPictureModal').modal('hide');
+    var metadata = {
+      caption: caption,
+      featured: featured
+    };
+
+    var file = new FS.File(file);
+    file.metadata = metadata;
+
+    Pictures.insert(file, function (error, file) {
+      if (error) 
+        alert(error.reason);
+      else {
+        alert(getError('picture-success'));
+        template.$('#addPictureModal').modal('hide');
       }
-    } else {
-      alert("Please fill in all the fields!");
+    });
+  },
+  'click .js-btn-edit-picture': function (event, template) {
+    currentPicture = this._id;
+
+    if (this.metadata) {
+      template.$('#js-edit-caption').val(this.metadata.caption);
+      console.log
+      template.$('#js-edit-featured').prop('checked', this.metadata.featured);
     }
+  },
+  'click #js-edit': function (event, template) {
+    var caption = template.find('#js-edit-caption').value;
+    var featured = template.find('#js-edit-featured').checked;
+
+    if (!stripHTML(caption))
+      return alert(getError('no-caption'));
+
+    var metadata = {
+      caption: caption,
+      featured: featured
+    };
+
+    Pictures.update(currentPicture, { $set: { 'metadata': metadata } });
+    alert(getError('picture-success'));
+    template.$('#editPictureModal').modal('hide');
   },
   'click .deletePicture': function () {
-    if(confirm("Are you sure you want to delete this picture?")) {
-      Octagon.Pictures.delete(this.source);
-    }
+    var confirm = confirm(getError('confirm-delete'));
+
+    if (confirm)
+      Meteor.call('deletePicture', this._id);
   }
 });
--- collections/pictures.js
+++ collections/pictures.js
@@ -1,52 +1,106 @@
-PictureSchema = new SimpleSchema({
-  _id: {
-    type: String,
-    optional: true
-  },
-  imageUrl: {
-    type: String
-  },
-  caption: {
-    type: String
-  },
-  featured: {
-    type: Boolean
+Pictures = new FS.Collection('pictures', {
+  stores: [ new FS.Store.FileSystem('pictures') ],
+  filter: {
+    allow: {
+      contentTypes: ['image/*'] // allow only images in this FS.Collection
+    }
   }
 });
-
-Pictures = new Mongo.Collection('pictures');
-Pictures.attachSchema(PictureSchema);
 
 Pictures.allow({
   insert: isAdminById,
   update: isAdminById,
-  remove: isAdminById
+  remove: isAdminById,
+  download: canViewById
 });
 
-Meteor.methods({
-  createPicture: function (picture) {
-    var user = Meteor.user();
+// PictureSchema = new SimpleSchema({
+//   _id: {
+//     type: String,
+//     optional: true
+//   },
+//   imageId: {
+//     type: String
+//   },
+//   caption: {
+//     type: String
+//   },
+//   featured: {
+//     type: Boolean
+//   }
+// });
 
-    if (!user || !isAdmin(user))
-      throw new Meteor.Error('no-permission', getError('no-permission'));
+// Pictures = new Mongo.Collection('pictures');
+// Pictures.attachSchema(PictureSchema);
 
-    return Pictures.insert(picture);
-  },
-  updatePicture: function (picture) {
-    var user = Meteor.user();
-    var pictureId = picture._id;
+// Pictures.allow({
+//   insert: isAdminById,
+//   update: isAdminById,
+//   remove: isAdminById
+// });
 
-    if (!user || !isAdmin(user))
-      throw new Meteor.Error('no-permission', getError('no-permission'));
+// Meteor.methods({
+//   createPicture: function (picture) {
+//     var user = Meteor.user();
 
-    Pictures.update(pictureId, { $set: picture });
-  },
-  deletePicture: function (pictureId) {
-    var user = Meteor.user();
+//     if (!user || !isAdmin(user))
+//       throw new Meteor.Error('no-permission', getError('no-permission'));
 
-    if (!user || !isAdmin(user))
-      throw new Meteor.Error('no-permission', getError('no-permission'));
+//     // if (!caption)
+//     //   throw new Meteor.Error('no-caption', getError('no-caption'));
 
-    Pictures.remove(pictureId);
-  }
-});
+//     // if (typeof file === 'undefined')
+//     //   throw new Meteor.Error('no-picture', getError('no-picture'));
+
+//     // var picture = {
+//     //   caption: caption,
+//     //   featured: featured
+//     // };
+
+//     // Images.insert(file, function (error, file) {
+//     //   if (error)
+//     //     throw new Meteor.Error('default', getError('default'));
+//     //   else
+//     //     picture.imageId = file._id;
+//     // });
+
+//     return Pictures.insert(picture);
+//   },
+//   updatePicture: function (pictureId, file, caption, featured) {
+//     var user = Meteor.user();
+
+//     if (!user || !isAdmin(user))
+//       throw new Meteor.Error('no-permission', getError('no-permission'));
+
+//     var picture = {
+//       caption: caption,
+//       featured: featured
+//     };
+
+//     if (typeof file !== 'undefined') {
+//       Images.insert(file, function (error, file) {
+//         if (error)
+//           throw new Meteor.Error('default', getError('default'));
+//         else
+//           picture.imageId = file._id;
+//       });
+
+//       var imageId = Pictures.findOne(pictureId).imageId;
+//       // storage adapter auto removes the corresponding file in fs
+//       Images.remove(imageId); 
+//     }
+
+//     Pictures.update(pictureId, { $set: picture });
+//   },
+//   deletePicture: function (pictureId) {
+//     var user = Meteor.user();
+
+//     if (!user || !isAdmin(user))
+//       throw new Meteor.Error('no-permission', getError('no-permission'));
+
+//     var imageId = Pictures.findOne(pictureId).imageId;
+//     Images.remove(imageId);
+
+//     Pictures.remove(pictureId);
+//   }
+// });
