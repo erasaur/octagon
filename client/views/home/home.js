c1946c80987f74ede350bb9b1052d8aaf50860d7
erasaur <erasaur@gmail.com>
2014-10-26 00:41:14 -0700

    Implement deleting posts

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -19,7 +19,10 @@ Template.home.events({
   },
   'click .js-delete-post': function () {
     if(confirm('Are you sure you want to delete ' + this.title + '?')) {
-      Octagon.Posts.delete(this._id);
+      Meteor.call('deletePost', this._id, function (error) {
+        if (error)
+          alert(error.reason);
+      });
       $('#editPostModal').modal('hide');
     }
   }
