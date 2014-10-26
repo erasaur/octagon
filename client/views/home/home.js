3ade743cde432d4806e7d63a54e0432639cb2e06
erasaur <erasaur@gmail.com>
2014-10-26 02:16:07 -0700

    Implement front page carousel

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,12 +1,17 @@
 Template.homeHeader.helpers({
+  isActive: function () { // set the first item to be active by default
+    return this.index === 0 ? 'active': '';
+  },
   pictures: function () {
-    return Pictures.find({ 'metadata.featured': true });
+    var pictures = Pictures.find({ 'metadata.featured': true }).fetch();
+    pictures = _.map(pictures, function (picture, index) {
+      picture.index = index;
+      return picture;
+    });
+
+    return pictures;
   }
 });
-
-Template.home.rendered = function () {
-  $('#homeCarousel').find('.item:first').addClass('active');
-};
 
 Template.home.helpers({
   posts: function () {
