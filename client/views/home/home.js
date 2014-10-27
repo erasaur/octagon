00fc1b67616ba6fbede18400e0fdb78af78e9bd3
erasaur <erasaur@gmail.com>
2014-10-27 02:02:06 -0700

    Separate postItem template, style list items

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,4 +1,4 @@
-Template.homeHeader.helpers({
+Template.homeCarousel.helpers({
   isActive: function () { // set the first item to be active by default
     return this.index === 0 ? 'active': '';
   },
@@ -16,20 +16,5 @@ Template.homeHeader.helpers({
 Template.home.helpers({
   posts: function () {
     return Posts.find({}, { sort: { 'date': -1 } });
-  }
-});
-
-Template.home.events({
-  'click .js-edit-post': function (event, template) {
-    Session.set('currentPost', this);
-  },
-  'click .js-delete-post': function () {
-    if(confirm('Are you sure you want to delete ' + this.title + '?')) {
-      Meteor.call('deletePost', this._id, function (error) {
-        if (error)
-          alert(error.reason);
-      });
-      $('#editPostModal').modal('hide');
-    }
   }
 });
