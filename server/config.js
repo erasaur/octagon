5979c4f53abd7c93088f379b8e324442c9789460
erasaur <erasaur@gmail.com>
2014-11-17 18:36:47 -0800

    Improve styling

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ Accounts.emailTemplates.siteName = siteName;
 Accounts.emailTemplates.from = fromName;
 Accounts.emailTemplates.resetPassword.text = function (user, url) {
 	return 'Hey there ' + user.profile.name + 
-  '!\n\n So I heard you lost your password! No worries. ' + 
+  '!\n\nSo I heard you lost your password! No worries. ' + 
   'To reset your password, simply click on the link below: \n\n' + 
   url + '\n\nCheers,\n\nYour loving webmaster';
 };
